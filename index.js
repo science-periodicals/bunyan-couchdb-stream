b271beb818c126da3e1cb93ee8cfc71c4635bf94
Sebastien Ballesteros <sebastien.ballesteros@gmail.com>
2015-09-17 11:20:16 -0400

    generate db if not exist close #1

--- index.js
+++ index.js
@@ -6,23 +6,47 @@ util.inherits(LogStream, Writable);
 
 function LogStream(db, opts) {
   this.db = db;
+  this.opts = opts || {};
+
+  this._createdDb = false;
 
   Writable.call(this, {objectMode: true});
 }
 
 LogStream.prototype._write = function(chunk, encoding, callback) {
-
   request.post({
     url: this.db,
+    auth: this.opts.auth,
     json: chunk
   }, function(err, resp, body) {
     if (err) return callback(err);
     if (resp.statusCode >= 400) {
-      callback(new Error(body ? body.reason: 'could not POST to CouchDB'));
+
+      if (!this._createdDb && resp.statusCode === 404 && body && body.reason === 'no_db_file') {
+        // try to create DB
+        request.put({
+          url: this.db,
+          auth: this.opts.auth,
+          json: true
+        }, function(err, resp, body) {
+          this._createdDb = true;
+          if (err) return callback(err);
+          if (resp.statusCode >= 400) {
+            return callback(new Error('Could not create db.' + (body ? (' ' + (body.reason || body.error)) : '')));
+          }
+
+          this._write(chunk, encoding, callback);
+
+        }.bind(this));
+
+      } else {
+        callback(new Error(body ? body.reason : 'could not POST to CouchDB'));
+      }
+
     } else {
       callback(null);
     }
-  });
+  }.bind(this));
 
 }
 
--- test/test.js
+++ test/test.js
@@ -11,44 +11,51 @@ describe('bunyan couchdb stream', function() {
   var db = 'http://127.0.0.1:5984/test-bunyan-couchdb-stream';
   var auth = {
     user: process.env.COUCH_ADMIN_USERNAME,
-    pass: process.env.COUCH_ADMIN_PASSWORD,
-  }
+    pass: process.env.COUCH_ADMIN_PASSWORD
+  };
 
-  before(function(done) {
-    request.put({
-      url: db,
-      auth: auth
-    }, function(err, resp, body) {
-      done();
-    });
-  });
+  it('should create a DB and write to CouchDB', function(done) {
 
-  it('should write to CouchDB', function(done) {
+    // we would need to do log.close() to be able to detect the
+    // `finish` event of the logStream but the close method is currently
+    // commented out see https://github.com/trentm/node-bunyan/issues/192
+    // so we use the changes feed to test
 
-    // we would need to do log.close() but the close method is currently commented out see https://github.com/trentm/node-bunyan/issues/192 so we use the changes feed to test
-    var feed = follow({
-      db: db,
-      include_docs: true
-    }, function(err, change) {
-      assert.equal(change.doc.hello, 'world');
-      assert.equal(change.doc.msg, 'hello world');
-      feed.stop();
-      done();
-    });
+    (function _follow() {
+
+      var feed = follow({
+        since: 0,
+        db: db,
+        include_docs: true
+      }, function(err, change) {
+        if (err) {
+          if (/no_db_file/.test(err.message)) {
+            return setTimeout(_follow, 500);
+          } else {
+            return console.error(err);
+          }
+        }
+        assert.equal(change.doc.foo, 'bar');
+        assert.equal(change.doc.msg, 'hello world');
+        feed.stop();
+        done();
+      });
+
+    })();
 
 
     var log = bunyan.createLogger({
       name: 'test',
       streams: [{
-        stream: new LogStream(db),
+        stream: new LogStream(db, {auth: auth}),
         type: 'raw'
       }],
     });
 
-
-    log.info({hello: 'world'}, 'hello world');
+    log.info({foo: 'bar'}, 'hello world');
 
   });
+
 
   after(function(done) {
     request.del({
